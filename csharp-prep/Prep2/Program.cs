09e2580dac861515b117688af722cf712e56c081
skittlz365 <nparr365@gmail.com>
2026-04-24 22:49:42 -0600

    Finished Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string napPercentage = Console.ReadLine();
+
+        char napLetterGrade = 'A';
+        int napPercentageNumber = int.Parse(napPercentage);
+
+        if(napPercentageNumber >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+
+        else
+        {
+            Console.WriteLine("Looks like you didn't pass the class. Better luck next time!");
+        }
+
+        if (napPercentageNumber >= 90)
+        {
+            napLetterGrade = 'A';
+        }
+        
+        else if(napPercentageNumber >= 80)
+        {
+            napLetterGrade = 'B';
+        }
+
+        else if(napPercentageNumber >= 70)
+        {
+            napLetterGrade = 'C';
+        }
+
+        else if(napPercentageNumber >= 60)
+        {
+            napLetterGrade = 'D';
+        }
+
+        else
+        {
+            napLetterGrade = 'F';
+        }
+        
+        int napPercentageRemainder = napPercentageNumber % 10;
+        char napPlusOrMinus;
+
+        if(napPercentageRemainder >= 7 && napPercentageNumber <= 93 && napPercentageNumber >= 60)
+        {
+            napPlusOrMinus = '+';
+        }
+
+        else if(napPercentageRemainder <= 3 && napPercentageNumber <= 93 && napPercentageNumber >= 60)
+        {
+            napPlusOrMinus = '-';
+        }
+
+        else
+        {
+            napPlusOrMinus = ' ';
+        }
+
+        Console.WriteLine($"Your grade is {napLetterGrade}{napPlusOrMinus}");
     }
 }

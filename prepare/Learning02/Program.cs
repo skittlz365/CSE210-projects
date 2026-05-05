b207f121ee5567605c0aaff76aa67bcb0b8f1b91
skittlz365 <nparr365@gmail.com>
2026-05-05 08:34:58 -0600

    Finished Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2016;
+        job1._endYear = 2026;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Design Engineer";
+        job2._startYear = 2021;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Nick";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.displayResume();
     }
 }

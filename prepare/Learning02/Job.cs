d994a1a40180fa16129a114a575ea951002140f6
skittlz365 <nparr365@gmail.com>
2026-05-05 08:35:19 -0600

    Finished classes

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,11 @@
+public class Job
+{
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+    public void display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
